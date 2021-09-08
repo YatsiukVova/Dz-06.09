a4dec8218bacbe0183607a6068dec6c91a60615b
Vova <vovan4ksuper@gmail.com>
2021-09-08 16:24:26 +0300

    Попытка №1

---
+++ ConsoleApp1/Logger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public static class Logger
+    {
+        private static string _str = "";
+        public static void Info()
+        {
+            _str += "{" + DateTime.Now+"}: {Info}: {Start method:Info}\n";
+            Console.WriteLine("{" + DateTime.Now + "}: {Info}: {Start method:Info}\n");
+        }
+        public static void Warning()
+        {
+            _str += "{" + DateTime.Now + "}: {Warning}: {Skipped logic in method:Warning}\n";
+            Console.WriteLine("{" + DateTime.Now + "}: {Warning}: {Skipped logic in method:Warning}\n");
+        }
+        public static void WriteFile()
+        {
+            File.WriteAllText("log.txt",_str);
+        }
+    }
+}
---
+++ ConsoleApp1/Starter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Starter
+    {
+        private int _counter;
+
+        public void Run()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                _counter = new Random().Next(1, 3);
+                if(_counter==1)
+                {
+                   Actions.A1();
+                }
+                else if(_counter==2)
+                {
+                    Actions.A2();
+                }
+                else
+                {
+                    Actions.A3();
+                }
+            }
+            Logger.WriteFile();
+          }
+    }
+}
